d753d844a315ff0e522b802ced013dd349c327e7
Eugene White <eugenewhite1994@gmail.com>
2020-07-08 18:36:44 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,8 +2,8 @@
 def unsafe?(speed)
   speed = 55
 if speed >= 60 ? true : false
-elsif speed <= 40 ? do true : false
-elsif speed >= 40 ? do false : true
+elsif speed <= 40 ? true : false
+elsif speed >= 40 ? false : true
 end
 end
 
